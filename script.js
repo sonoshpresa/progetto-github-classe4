14ce7c4c2e47ee7e12e31ca3dd33160a653c34aa
shpresa <aricimichy@gmail.com>
2026-05-23 14:31:17 +0200

    Nuova pagina web
    Aggiunta pagina html, css e JavaScript

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", () => {
+    const title = document.getElementById("title");
+    title.textContent = "Hai cliccato il pulsante!";
+    title.style.color = "#ffeaa7";
+});
